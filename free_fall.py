9d14b9a9f883ffee963cc39b29666c065c0b54cc
R morel <38894494+mrl893@users.noreply.github.com>
2024-02-24 23:33:34 -0500

    Update free_fall.py

--- free_fall.py
+++ free_fall.py
@@ -13,3 +13,14 @@ def main()->str:
 if __name__ == "__main__":
     print(main())
     
+INITIAL_VELOCITY = 60
+ACCELERATION_OF_GRAVITY = 9.81
+
+
+
+
+
+TIME  = 0.6
+VPB = INITIAL_VELOCITY * TIME -\
+    0.5*ACCELERATION_OF_GRAVITY*TIME**2
+print(VPB) # 34.2342
